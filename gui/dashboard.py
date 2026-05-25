7d02d4fdb96c2ab3db6069048f6bbd6f26381469
Chhavi Ambasta <chhaviambasta@gmail.com>
2026-05-25 22:36:06 +0530

    Hashguard dashboard update

--- gui/dashboard.py
+++ gui/dashboard.py
@@ -4,6 +4,11 @@ import hashlib
 import os
 from datetime import datetime
 import sqlite3
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import letter
+import os
+import shutil
 
 current_user = ""
 
@@ -55,6 +60,76 @@ title.pack(pady=20)
 # ================= BUTTON FRAME =================
 button_frame = tk.Frame(root, bg="#0b1437")
 button_frame.pack(pady=20)
+
+# ================= SEARCH FUNCTION =================
+
+def search_file():
+
+    search = search_entry.get().lower()
+
+    for item in tree.get_children():
+
+        values = tree.item(item)["values"]
+
+        if search in str(values[0]).lower():
+
+            tree.selection_set(item)
+            tree.focus(item)
+
+            break
+
+# ================= SEARCH BAR =================
+
+search_frame = tk.Frame(root, bg="#0b1437")
+search_frame.pack(pady=10)
+
+search_entry = tk.Entry(
+    search_frame,
+    font=("Segoe UI", 11),
+    width=30
+)
+
+search_entry.grid(row=0, column=0, padx=10)
+
+search_btn = tk.Button(
+    search_frame,
+    text="Search",
+    command=search_file,
+    font=("Segoe UI", 10, "bold"),
+    bg="#4f46e5",
+    fg="white",
+    padx=15,
+    pady=5,
+    borderwidth=0,
+    cursor="hand2"
+)
+
+search_btn.grid(row=0, column=1)
+
+# ================= LOGOUT FUNCTION =================
+
+def logout():
+
+    root.destroy()
+
+    import login
+
+# ================= LOGOUT BUTTON =================
+
+logout_btn = tk.Button(
+    header,
+    text="Logout",
+    command=logout,
+    font=("Segoe UI", 10, "bold"),
+    bg="#dc2626",
+    fg="white",
+    padx=15,
+    pady=5,
+    borderwidth=0,
+    cursor="hand2"
+)
+
+logout_btn.place(x=1120, y=25)
 
 # ================= HASH FUNCTION =================
 def generate_hash(file_path):
@@ -143,8 +218,23 @@ for col in columns:
 
 tree.pack(fill="both", expand=True, padx=20, pady=10)
 
+# ================= FILE COUNT =================
+
+count_label = tk.Label(
+    root,
+    text="Total Files: 0",
+    font=("Segoe UI", 11, "bold"),
+    fg="white",
+    bg="#0b1437"
+)
+
+count_label.pack(pady=5, before=table_container)
+
+
 # ================= LOAD FILES =================
 def load_files():
+
+    tree.delete(*tree.get_children())
 
     cursor.execute("SELECT * FROM files")
 
@@ -163,6 +253,10 @@ def load_files():
                 row[5]
             )
         )
+
+    count_label.config(
+        text=f"Total Files: {len(tree.get_children())}"
+    )
 
 # ================= UPLOAD FILE =================
 def upload_file():
@@ -186,6 +280,14 @@ def upload_file():
         upload_time = datetime.now().strftime(
             "%d-%m-%Y %H:%M:%S"
         )
+
+        # Create uploads folder if not exists
+        if not os.path.exists("uploads"):
+            os.makedirs("uploads")
+            
+        # Copy file to uploads folder
+        destination = os.path.join("uploads", file_name)
+        shutil.copy(file_path, destination)
 
         # Duplicate Check
         cursor.execute(
@@ -232,18 +334,8 @@ def upload_file():
 
         conn.commit()
 
-        # Show in Table
-        tree.insert(
-            "",
-            "end",
-            values=(
-                file_name,
-                f"{file_size} KB",
-                file_hash[:30] + "...",
-                current_user,
-                upload_time
-            )
-        )
+        # Reload Table
+        load_files()
 
         status_label.config(
             text="File Uploaded Successfully",
@@ -256,6 +348,25 @@ def upload_file():
             "Error",
             str(e)
         )
+
+def open_file():
+
+    selected = tree.focus()
+
+    if not selected:
+        messagebox.showwarning("Warning", "Please select a file")
+        return
+
+    values = tree.item(selected, "values")
+
+    filename = values[0]
+
+    filepath = os.path.join("uploads", filename)
+
+    if os.path.exists(filepath):
+        os.startfile(filepath)
+    else:
+        messagebox.showerror("Error", "File not found")
 
 # ================= VERIFY FILE =================
 def verify_file():
@@ -321,6 +432,135 @@ def verify_file():
             str(e)
         )
 
+open_btn = tk.Button(
+    button_frame,
+    text="Open File",
+    command=open_file,
+    bg="#ebde25",
+    fg="white",
+    font=("Segoe UI", 12, "bold"),
+    width=15,
+    height=2,
+    relief="flat",
+    cursor="hand2"
+)
+
+open_btn.grid(row=0, column=4, padx=15)
+
+# ================= DELETE FILE =================
+
+def delete_file():
+
+    selected_item = tree.selection()
+
+    if not selected_item:
+
+        messagebox.showwarning(
+            "No Selection",
+            "Please select a file to delete"
+        )
+
+        return
+
+    values = tree.item(selected_item)["values"]
+
+    file_name = values[0]
+
+    confirm = messagebox.askyesno(
+        "Delete File",
+        f"Are you sure you want to delete:\n{file_name} ?"
+    )
+
+    if confirm:
+
+        cursor.execute(
+            "DELETE FROM files WHERE file_name=?",
+            (file_name,)
+        )
+
+        conn.commit()
+
+        load_files()
+
+        status_label.config(
+            text="File Deleted Successfully",
+            fg="red"
+        )
+
+# ================= EXPORT REPORT =================
+
+def export_pdf():
+
+    cursor.execute("SELECT * FROM files")
+
+    rows = cursor.fetchall()
+
+    if not rows:
+
+        messagebox.showwarning(
+            "No Data",
+            "No files available to export"
+        )
+
+        return
+
+    pdf = SimpleDocTemplate(
+        "HashGuard_Report.pdf",
+        pagesize=letter
+    )
+
+    data = [
+        [
+            "File Name",
+            "Size",
+            "Uploaded By",
+            "Date & Time"
+        ]
+    ]
+
+    for row in rows:
+
+        data.append([
+            row[1],
+            row[2],
+            row[4],
+            row[5]
+        ])
+
+    table = Table(data)
+
+    style = TableStyle([
+
+        ('BACKGROUND', (0, 0), (-1, 0), colors.darkblue),
+
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.white),
+
+        ('GRID', (0, 0), (-1, -1), 1, colors.black),
+
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 10),
+
+        ('BACKGROUND', (0, 1), (-1, -1), colors.whitesmoke)
+
+    ])
+
+    table.setStyle(style)
+
+    elements = [table]
+
+    pdf.build(elements)
+
+    status_label.config(
+        text="Report Exported Successfully",
+        fg="#22c55e"
+    )
+
+    messagebox.showinfo(
+        "Success",
+        "Report Saved as HashGuard_Report.pdf"
+    )
+
 # ================= BUTTONS =================
 upload_btn = tk.Button(
     button_frame,
@@ -360,6 +600,48 @@ verify_btn.grid(
     padx=15
 )
 
+# ================= DELETE BUTTON =================
+
+delete_btn = tk.Button(
+    button_frame,
+    text="Delete File",
+    font=("Segoe UI", 12, "bold"),
+    bg="#dc2626",
+    fg="white",
+    padx=35,
+    pady=12,
+    borderwidth=0,
+    cursor="hand2",
+    command=delete_file
+)
+
+delete_btn.grid(
+    row=0,
+    column=2,
+    padx=15
+)
+
+# ================= EXPORT REPORT BUTTON =================
+
+pdf_btn = tk.Button(
+    button_frame,
+    text="Export Report",
+    font=("Segoe UI", 12, "bold"),
+    bg="#2563eb",
+    fg="white",
+    padx=35,
+    pady=12,
+    borderwidth=0,
+    cursor="hand2",
+    command=export_pdf
+)
+
+pdf_btn.grid(
+    row=0,
+    column=3,
+    padx=15
+)
+
 # ================= LOAD SAVED FILES =================
 load_files()
 
@@ -373,4 +655,3 @@ footer = tk.Label(
 )
 
 footer.pack(pady=10)
-
